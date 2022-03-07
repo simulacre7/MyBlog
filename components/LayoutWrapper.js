c9035b3a877429443b34864b45d40d114bfc80e0
Kihwan Kim <juljin1875@gmail.com>
2022-03-07 20:09:44 +0900

    Modify logo images

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -6,8 +6,10 @@ import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import { useTheme } from 'next-themes'
 
 const LayoutWrapper = ({ children }) => {
+  const { theme, resolvedTheme } = useTheme()
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
@@ -16,7 +18,7 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  <Logo fill="currentColor" className="text-gray-900 dark:text-gray-100" />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
