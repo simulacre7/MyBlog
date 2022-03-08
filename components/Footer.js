5db2f5e8c87ab781ea4cf2e336a38d238da03e14
Kihwan Kim <juljin1875@gmail.com>
2022-03-08 19:18:45 +0900

    Add my CV page link & icon

--- components/Footer.js
+++ components/Footer.js
@@ -13,18 +13,13 @@ export default function Footer() {
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size="6" />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
+          <SocialIcon kind="cv" href={siteMetadata.cv} size="6" />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          Copyright {`© ${new Date().getFullYear()} ${siteMetadata.author}.`} All rights reserved.
         </div>
       </div>
     </footer>
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -16,6 +16,7 @@ const siteMetadata = {
   //facebook: 'https://facebook.com',
   //youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com/in/1875',
+  cv: 'https://simulacre7.github.io/resume',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
